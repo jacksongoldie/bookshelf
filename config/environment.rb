b619988792320269293c9a03645d66738e710e6f
jacksongoldie <goldiejackson25@yahoo.com>
2023-02-02 16:43:23 -0600

    download pg

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,7 @@
 # Load the Rails application.
 require_relative "application"
 
+DATABASE_URL="postgres://xesqpbvd:XFdqTNTfMcCbPQsvslsM5A_V43IePRZN@kashin.db.elephantsql.com/xesqpbvd"
+
 # Initialize the Rails application.
 Rails.application.initialize!
