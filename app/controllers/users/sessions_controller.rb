41f6c5ce820dea9c5decdf72f2d6acc34403f5e8
jacksongoldie <goldiejackson25@yahoo.com>
2023-02-09 11:12:01 -0600

    add [] toarray to session cont

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -7,18 +7,18 @@ class Users::SessionsController < Devise::SessionsController
       private
 
       def respond_with(resource, _opts = {})
-        render json: {
+        render json: [{
           status: {code: 200, message: 'Logged in sucessfully.'},
           data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
-        }, status: :ok
+        }], status: :ok
       end
   
     def respond_to_on_destroy
       if current_user
-        render json: {
+        render json: [{
           status: 401,
           message: "Couldn't find an active session."
-        }, status: :unauthorized
+        }], status: :unauthorized
       else
         render json: {
           status: 200,
